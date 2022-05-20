f13df04f8fb2e77fd19cfe74f20f8a5e60db6042
Neeraj Yadav <yneeraj9111@gmail.com>
2022-05-20 18:00:16 +0530

    chnages

--- front/src/components/PetStatus.jsx
+++ front/src/components/PetStatus.jsx
@@ -115,6 +115,8 @@ export default function PetStatus() {
                         {el.status}
                       </Button>
                     )}
+
+                    
                   </StyledTableCell>
                 </StyledTableRow>
               ))}
